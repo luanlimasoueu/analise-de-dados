d6026970374e39edee0e2937e759c5a325c7764f
okluan <luanemerson8@gmail.com>
2026-01-19 23:59:40 -0300

    Study

---
+++ livros/Machine_Learning_Algorithms_From_Scratch/1_chapter/1_3_chapter.py
@@ -0,0 +1,11 @@
+# Example of converting string variables to float
+from csv import reader
+# Load a CSV file
+def load_csv(filename):
+    dataset = list()
+    return
+
+filename = 'pima-indians-diabetes.csv'
+dataset = load_csv(filename)
+
+
